298769fa3f358e0b083ed82c288e3961b4ed7c43
Danial Khilji <56604788+danialkhilji@users.noreply.github.com>
2022-06-06 18:41:33 +0100

    Update streamlit_app.py
    Adding fruit name instead of fruit index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Dinner")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -12,7 +12,8 @@ streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruits they want to include
 streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index))
