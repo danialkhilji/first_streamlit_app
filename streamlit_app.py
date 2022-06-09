762bc7e1b1fcabb0084d0fe07c9ac458ede146a9
Danial Khilji <56604788+danialkhilji@users.noreply.github.com>
2022-06-09 17:15:07 +0100

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,15 @@ import requests
 # New section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!') # just writes data to the screen
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 streamlit.title("My Mom's New Healthy Dinner")
 
