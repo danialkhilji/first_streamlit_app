a632599c6b93ecd90a20fa792a15029c20e8be4b
Danial Khilji <56604788+danialkhilji@users.noreply.github.com>
2022-06-09 17:02:59 +0100

    Update streamlit_app.py
    Added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 import pandas
 import requests
 
+# New section to display fruitvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.title("My Mom's New Healthy Dinner")
 
